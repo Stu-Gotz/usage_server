2c0bdd2d400f65beb8a56c340ef6e5512ef2f175
Stu-Gotz <gamer691@gmail.com>
2021-04-19 18:29:08 +0100

    same

--- db.js
+++ db.js
@@ -8,6 +8,9 @@ const connectionString = process.env.NODE_ENV === "production"
   ? process.env.DATABASE_URL
   : `postgresql://${process.env.PG_USER}:${process.env.PG_PASS}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
-const conn = new Pool({ connectionString });
+const conn = new Pool({ 
+  connectionString: connectionString,
+  ssl: { rejectUnauthorized: false }
+});
 
 module.exports = conn;
