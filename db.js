fcfb978c498c0d426306ff0af4513f1246fbd985
Stu-Gotz <gamer691@gmail.com>
2021-04-19 14:29:26 +0100

    changed conn string in db connect

--- db.js
+++ db.js
@@ -1,13 +1,17 @@
 const { Pool } = require('pg');
 require("dotenv").config();
 
-const devConfig = {
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASS,
-  port: process.env.PG_PORT
-};
+// const devConfig = {
+//   user: process.env.PG_USER,
+//   host: process.env.PG_HOST,
+//   database: process.env.PG_DATABASE,
+//   password: process.env.PG_PASS,
+//   port: process.env.PG_PORT
+// };
+
+const devConfig = `postgresql://${process.env.PG_USER}:
+${process.env.PG_PASSWORD}@${process.env.PG_HOST}:
+${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
 const proConfig = {
   connectionString: process.env.DATABASE_URL
