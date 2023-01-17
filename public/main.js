6a92049eddb651c1cfaed9228fd98e9c96aabcec
Alan Nardo <alan.nardo.91@gmail.com>
2023-01-17 20:48:25 +0000

    finally got styling how i like it, next step maybe work on a printout for the serve, also update server

--- public/main.js
+++ public/main.js
@@ -1,14 +1,45 @@
 let data = {}
 
-function fetchAPI() {
-  data = {}
+function getMonth() {
+  const d = new Date();
+  let month = d.getUTCMonth() + 1;
+  let year = d.getUTCFullYear();
+
+  month = month.toString();
+  month = `0${month}`;
+
+  valueString = `${year}-${month}`;
+  console.log(valueString)
+  return valueString;
+}
+
+// document.getElementById('date-input').value = getMonth();
+
+
+
+async function fetchAPI() {
+  
   const baseURL = "https://usage-server.herokuapp.com/data"
   const date = document.getElementById("date-input").value;
   const tier = document.getElementById("tier-input").value;
 
-  window.location.replace(`${baseURL}/${date}/${tier}`)
+  const url = `${baseURL}/${date}/${tier}`;
+
+  const res = await fetch(url);
+
+  const data = await res.json();
+  // console.log(data.data)
+  // for(const [key, value] of Object.entries(data.data)){
+  //   console.log(`${key}: ${value}`);
+  // }
+  const results = new Array();
+  results.push(data.data);
+  console.log(results)
+  return results
 }
 
+
 function resetForm(){
-  document.getElementById("userForm").reset();
+  document.getElementById("date-input").value = getMonth();
+  document.getElementById("tier-input").value = "";
 }
